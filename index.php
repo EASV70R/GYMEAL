f392890da1b303078a2b64b333b833e360be1122
Tor <75667950+EASVT0R@users.noreply.github.com>
2022-11-09 19:08:58 +0100

    big changes (alot broken) - part 2

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ $router->get('/', 'views/index');
 $router->get('home', 'views/index');
 $router->get('about', 'views/about');
 $router->get('contactus', 'views/contactus');
+$router->get('products', 'views/products');
+$router->get('productview', 'views/productview');
 $router->get('login', 'views/login');
 $router->get('register', 'views/register');
 $router->get('profile', 'views/profile');
@@ -16,10 +18,10 @@ $router->get('profilesettings', 'views/profilesettings');
 $router->get('admin', 'admin/index');
 $router->get('admin', 'admin/adminsettings');
 $router->get('admin', 'admin/invoice');
+$router->get('admin', 'admin/blog');
 $router->get('logout', 'views/logout');
 $router->get('404', 'views/404');
-$router->get('products', 'views/products');
-$router->get('productview', 'views/productview');
+
 
 
 
