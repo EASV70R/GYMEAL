f4c98feac0e64483fc509e98f30a7b1060ca3134
Tor <75667950+EASVT0R@users.noreply.github.com>
2022-11-12 17:24:50 +0100

    more updates to product listing

--- views/products.php
+++ views/products.php
@@ -53,8 +53,8 @@ Util::Navbar();
                     <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="product-item">
                             <div class="position-relative bg-light overflow-hidden">
-                                <img class="img-fluid w-100" src="<?= Util::Print($row->image); ?>" alt="">
-                                <!-- <div class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div> -->
+                                <img class="img-wrapper" src="<?= Util::Print($row->image); ?>" alt="">
+                                <!-- img-fluid w-100 <div class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div> -->
                             </div>
                             <div class="text-center p-4">
                                 <a class="d-block h5 mb-2" href=""><?= Util::Print($row->title); ?></a>
@@ -62,7 +62,9 @@ Util::Navbar();
                             </div>
                             <div class="d-flex border-top">
                                 <small class="w-50 text-center border-end py-2">
-                                    <a class="text-body" href="<?= (SITE_URL); ?>/productview/?product&id=<?= Util::Print($row->productId); ?>"><i class="fa fa-eye text-primary me-2"></i>View
+                                    <a class="text-body"
+                                        href="<?= (SITE_URL); ?>/productview/?product&id=<?= Util::Print($row->productId); ?>"><i
+                                            class="fa fa-eye text-primary me-2"></i>View
                                         detail</a>
                                 </small>
                                 <small class="w-50 text-center py-2">
@@ -80,22 +82,24 @@ Util::Navbar();
             </div>
             <div id="tab-2" class="tab-pane fade show p-0">
                 <div class="row g-4">
-                <?php foreach ($drinkFilter as $row) : ?>
+                    <?php foreach ($drinkFilter as $row) : ?>
                     <div class="col-xl-3 col-lg-4 col-md-6">
                         <div class="product-item">
                             <div class="position-relative bg-light overflow-hidden">
-                                <img class="img-fluid w-100" src="<?= Util::Print($row->image); ?>" alt="">
+                                <img class="img-wrapper" src="<?= Util::Print($row->image); ?>" alt="">
                                 <div
                                     class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
                                     New</div>
                             </div>
                             <div class="text-center p-4">
                                 <a class="d-block h5 mb-2" href=""><?= Util::Print($row->title); ?></a>
-                                <span class="text-primary me-1"><?= Util::Print($row->price); ?></span>
+                                <span class="text-primary me-1">$<?= Util::Print($row->price); ?></span>
                             </div>
                             <div class="d-flex border-top">
                                 <small class="w-50 text-center border-end py-2">
-                                    <a class="text-body" href="<?= (SITE_URL); ?>/productview/?product&id=<?= Util::Print($row->productId); ?>"><i class="fa fa-eye text-primary me-2"></i>View
+                                    <a class="text-body"
+                                        href="<?= (SITE_URL); ?>/productview/?product&id=<?= Util::Print($row->productId); ?>"><i
+                                            class="fa fa-eye text-primary me-2"></i>View
                                         detail</a>
                                 </small>
                                 <small class="w-50 text-center py-2">
@@ -128,7 +132,9 @@ Util::Navbar();
                             </div>
                             <div class="d-flex border-top">
                                 <small class="w-50 text-center border-end py-2">
-                                    <a class="text-body" href="<?= (SITE_URL); ?>/productview/?product&id=<?= Util::Print($row->productId); ?>"><i class="fa fa-eye text-primary me-2"></i>View
+                                    <a class="text-body"
+                                        href="<?= (SITE_URL); ?>/productview/?product&id=<?= Util::Print($row->productId); ?>"><i
+                                            class="fa fa-eye text-primary me-2"></i>View
                                         detail</a>
                                 </small>
                                 <small class="w-50 text-center py-2">
