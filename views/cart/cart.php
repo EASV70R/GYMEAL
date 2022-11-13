083feb02e18ee1fe147b1727f471edcc16185c0f
Tor <75667950+EASVT0R@users.noreply.github.com>
2022-11-13 18:03:57 +0100

    SHOPPING CART WIP

--- views/cart/cart.php
+++ views/cart/cart.php
@@ -2,100 +2,70 @@
 require_once './cms/require.php';
 require_once './cms/controllers/company.php';
 
-require_once './cms/controllers/cart.php';
+Util::Header();
+Util::Navbar();
 
-$cart = new Cart();
-if (isset($_SESSION["cartItem"])) {
-    $cartController->existingCart($_SESSION["cartItem"]);
-}
-if(!empty($_GET["action"])) {
-    if (isset($_GET['code'])){
-        $code = $_GET['code'];}
-
-    //start the switch/case
-    switch($_GET["action"]) {
-    //adding items to cart
-	    case "add":
-	        $cartController->cartAdd($code, $_POST["quantity"]);
-            $_SESSION  = $cartController->itemArray;
-	    break;
-    //Remove item from cart
-	    case "remove":
-	        $cartController->cartRemove($code);
-            $_SESSION  = $cartController->itemArray;
-            break;
-    //Empty the entire cart
-	    case "empty":
-		    unset($_SESSION["cartItem"]);
-	    break;
-    }
-}
 ?>
-<Html>
-<head>
-<Title>PHP Shopping Cart</Title>
-<link href="style.css" type="text/css" rel="stylesheet" />
-</head>
-<Body>
-<div id="shopping-cart">
-<div class="heading">Shopping Cart <a id="emptyBtn" href="index.php?action=empty">Empty Cart</a></div>
-<?php
-//Reset total cost to do recalc
-if(isset($_SESSION["cartItem"])){
-    $item_total = 0;
-?>	
-<table cellpadding="10" cellspacing="1">
-<tbody>
-<tr>
-<th><strong>Name</strong></th>
-<th><strong>Code</strong></th>
-<th><strong>Quantity</strong></th>
-<th><strong>Price</strong></th>
-<th><strong>Action</strong></th>
-</tr>	
-<?php		
-    foreach ($_SESSION["cartItem"] as $item){
-		?>
-				<tr>
-				<td><strong><?php echo $item["name"]; ?></strong></td>
-				<td><?php echo $item["code"]; ?></td>
-				<td><?php echo $item["quantity"]; ?></td>
-				<td><?php echo $item["price"]." DKK"; ?></td>
-				<td><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="removeBtn">Remove</a></td>
-				</tr>
-				<?php
-        $item_total += ($item["price"]*$item["quantity"]);
-		}
-		?>
 
-<tr>
-<td colspan="5" align=right><strong>Total:</strong> <?php echo $item_total." DKK"; ?></td>
-</tr>
-</tbody>
-</table>		
-  <?php
-}
-?>
-</div>
-<div class="heading">Products</div>
-	<?php
-	$product_array = $cart->GetCartArray();
-	if (!empty($product_array)) { 
-		foreach($product_array as $aNumber=> $value){
-	?>
-		<div class="product-item">
-			<form method="post" action="index.php?action=add&code=<?php echo $product_array[$aNumber]["code"]; ?>">
-			<div class="product-image"><img src="<?php echo $product_array[$aNumber]["image"]; ?>"></div>
-			<div><strong><?php echo $product_array[$aNumber]["name"]; ?></strong></div>
-			<div class="product-price"><?php echo $product_array[$aNumber]["price"]." DKK"; ?></div>
-			<div>
-                <input type="text" name="quantity" value="1" size="2" />
-                <input type="submit" value="Add to cart" class="addBtn" /></div>
-			</form>
-		</div>
-	<?php
-		}
-	}
-	?>
-</Body>
-</Html>
+</br>
+</br>
+<main class="container mt-5">
+    <div class="col-12 mt-3 mb-2">
+        <div class="alert alert-primary" role="alert">
+            WIP
+        </div>
+    </div>
+    <div class="container padding-bottom-3x mb-1">
+        <div class="table-responsive shopping-cart">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th class="text-center">Quantity</th>
+                        <th class="text-center">Subtotal</th>
+                        <th class="text-center">Discount</th>
+                        <th class="text-center"><a class="btn btn-sm btn-outline-danger" href="#">Clear Cart</a></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="product-item">
+                                <a class="product-thumb" href="#"><img
+                                        src="https://via.placeholder.com/220x180/FF0000/000000" alt="Product"></a>
+                                <div class="product-info">
+                                    <h4 class="product-title"><a href="#">test</a></h4>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="text-center">
+                            <div class="count-input">
+                                <select class="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                        </td>
+                        <td class="text-center text-lg text-medium">$43.90</td>
+                        <td class="text-center text-lg text-medium">$18.00</td>
+                        <td class="text-center"><a class="remove-from-cart" href="#" data-toggle="tooltip" title=""
+                                data-original-title="Remove item"><i class="fa fa-trash"></i></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="shopping-cart-footer">
+            <div class="column"><a class="btn btn-outline-secondary" href="#"><i class="icon-arrow-left"></i>&nbsp;Back
+                    to Shopping</a></div>
+            <div class="column"><a class="btn btn-primary" href="#" data-toast="" data-toast-type="success"
+                    data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Your cart"
+                    data-toast-message="is updated successfully!">Update Cart</a><a class="btn btn-success"
+                    href="#">Checkout</a></div>
+        </div>
+    </div>
+</main>
+
+<?php Util::Footer(); ?>
