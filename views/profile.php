b703190231185661676b6b1661be9797ddf5c17c
apdo-25 <72491666+Apdo-25@users.noreply.github.com>
2022-11-26 17:19:46 +0100

    cleaning up profile code for rework

--- views/profile.php
+++ views/profile.php
@@ -4,19 +4,6 @@ require_once './cms/controllers/company.php';
 
 Util::IsLoggedIn();
 
-require_once './cms/controllers/invoices.php';
-
-$invoice = new Invoices;
-$invoices = $invoice->GetUserInvoices();
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (isset($_GET["cancel"])) {
-        $invoiceId = $_GET["invoiceId"];
-        $invoice->DeleteInvoice($invoiceId, Session::Get('uid'));
-        Util::Redirect('/profile');
-    }
-}
-
 Util::Header();
 Util::Navbar();
 
@@ -39,58 +26,11 @@ Util::Navbar();
                     <h4 class="card-title text-center">Profile</h4>
                     <figcaption class="info">
                         <h6 class="title"><?= Util::Print(Session::Get('username'));?></h6>
-                        <p>Email: <?= Util::Print(Session::Get('email'));?>, Phone: <?= Util::Print(Session::Get('phone'));?>
+                        <p>Email: <?= Util::Print(Session::Get('email'));?>
                             <a href="#" class="px-2"><i class="fa fa-pen"></i></a>
                         </p>
                     </figcaption>
                     <hr>
-                    <h4 class="card-title text-center">Invoices</h4>
-                    <?php foreach ($invoices as $row) : ?>
-                    <article class="card border-primary mb-4">
-                        <div class="card-body">
-                            <header class="d-lg-flex">
-                                <div class="flex-grow-1">
-                                    <h6 class="mb-0">Invoice ID: <?= Util::Print($row->invoiceId); ?><i class="dot"></i>
-                                    <?php if ($row->status == 0) : ?>
-                                        <span class="text-danger"><?= Util::Print($invoice->GetInvoiceStatus($row->invoiceId, $row->userId)->status); ?></span>
-                                    <?php else : ?>
-                                        <span class="text-success"><?= Util::Print($invoice->GetInvoiceStatus($row->invoiceId, $row->userId)->status); ?></span>
-                                    <?php endif; ?>
-                                    </h6>
-                                    <span class="text-muted">Date: <?= Util::Print($row->createdAt); ?></span>
-                                </div>
-                                <div>
-                                    <a href="<?= (BASE_PATH); ?>profile.php?cancel=&invoiceId=<?= Util::Print($row->invoiceId); ?>"
-                                        class="btn btn-sm btn-outline-danger">Cancel order</a>
-                                    <a href="#" class="btn btn-sm btn-primary">Track order</a>
-                                </div>
-                            </header>
-                            <hr>
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <p class="mb-0 text-muted">Shipping address:</p>
-                                    <p class="m-0"><?= Util::Print($row->country); ?><br>
-                                    <?= Util::Print($row->city); ?> <?= Util::Print($row->region); ?>, <?= Util::Print($row->address); ?></p>
-                                </div>
-                            </div>
-                            <hr>
-                            <ul class="row">
-                                <li class="col-xl-4  col-lg-6">
-                                    <figure class="itemside mb-3">
-                                        <div class="aside">
-                                            <img width="72" height="72" src="" alt="test"
-                                                class="img-sm rounded border">
-                                        </div>
-                                        <figcaption class="info">
-                                            <p class="title"><?= Util::Print($row->itemName); ?></p>
-                                            <strong> $<?= Util::Print($row->price); ?> </strong>
-                                        </figcaption>
-                                    </figure>
-                                </li>
-                            </ul>
-                        </div>
-                    </article>
-                    <?php endforeach; ?>
                 </div>
             </div>
         </div>
