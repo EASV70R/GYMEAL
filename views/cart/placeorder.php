a410a08045bc0b16171d0a59b9deec12dee9df5a
EASV70R <75667950+EASV70R@users.noreply.github.com>
2022-12-04 17:28:29 +0100

    checkout -> placeorder

--- views/cart/placeorder.php
+++ views/cart/placeorder.php
@@ -5,7 +5,7 @@ require_once './cms/controllers/company.php';
 Util::Header();
 Util::Navbar();
 ?>
-<section class="checkout">
+<section class="placeorder">
     <div class="content-wrapper">
         <h1>Your Order Has Been Placed</h1>
         <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
