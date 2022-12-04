f5e83737934a10f73a362f0cef00485a0133e089
EASV70R <75667950+EASV70R@users.noreply.github.com>
2022-12-04 17:54:05 +0100

    setting margin to forms

--- views/login.php
+++ views/login.php
@@ -11,32 +11,34 @@ Util::Navbar();
 ?>
 
 <main class="container mt-2">
-    <div class="row justify-content-center">
-        <div class="col-12 mt-3 mb-2">
-            <?php if (isset($response)) : ?>
-            <div class="alert alert-primary" role="alert">
-                <?= Util::Print($response); ?>
+    <div class="customform">
+        <div class="row justify-content-center">
+            <div class="col-12 mt-3 mb-2">
+                <?php if (isset($response)) : ?>
+                <div class="alert alert-primary" role="alert">
+                    <?= Util::Print($response); ?>
+                </div>
+                <?php endif; ?>
             </div>
-            <?php endif; ?>
-        </div>
-        <div class="col-xl-3 col-lg-4 col-md-5 col-sm-7 col-xs-12 my-3">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title text-center">Login</h4>
-                    <form method="POST">
-                        <div class="form-group">
-                            <input type="text" class="form-control form-control-sm" placeholder="Username"
-                                name="username" required>
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control form-control-sm" placeholder="Password"
-                                name="password" required>
-                        </div>
-                        <button class="btn btn-outline-primary btn-block" name="login" id="submit" type="submit"
-                            value="submit">
-                            Login
-                        </button>
-                    </form>
+            <div class="col-xl-3 col-lg-4 col-md-5 col-sm-7 col-xs-12 my-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title text-center">Login</h4>
+                        <form method="POST">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" placeholder="Username"
+                                    name="username" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-sm" placeholder="Password"
+                                    name="password" required>
+                            </div>
+                            <button class="btn btn-outline-primary btn-block" name="login" id="submit" type="submit"
+                                value="submit">
+                                Login
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- views/register.php
+++ views/register.php
@@ -11,40 +11,42 @@ Util::Navbar();
 ?>
 
 <main class="container mt-2">
-    <div class="row justify-content-center">
-        <div class="col-12 mt-3 mb-2">
-            <?php if (isset($response)) : ?>
-            <div class="alert alert-primary" role="alert">
-                <?= Util::Print($response); ?>
+    <div class="customform">
+        <div class="row justify-content-center">
+            <div class="col-12 mt-3 mb-2">
+                <?php if (isset($response)) : ?>
+                <div class="alert alert-primary" role="alert">
+                    <?= Util::Print($response); ?>
+                </div>
+                <?php endif; ?>
             </div>
-            <?php endif; ?>
-        </div>
-        <div class="col-xl-3 col-lg-4 col-md-5 col-sm-7 col-xs-12 my-3">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title text-center">Register</h4>
-                    <form method="POST" action="<?= Util::Print($_SERVER['REQUEST_URI']); ?>">
-                        <div class="form-group">
-                            <input type="text" class="form-control form-control-sm" placeholder="Username"
-                                name="username" minlength="3" required>
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control form-control-sm" placeholder="Password"
-                                name="password" minlength="4" required>
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control form-control-sm" placeholder="Confirm password"
-                                name="confirmPassword" minlength="4" required>
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control form-control-sm" placeholder="email" name="email"
-                                required>
-                        </div>
-                        <button class="btn btn-outline-primary btn-block" name="register" id="submit" type="submit"
-                            value="submit">
-                            Register
-                        </button>
-                    </form>
+            <div class="col-xl-3 col-lg-4 col-md-5 col-sm-7 col-xs-12 my-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title text-center">Register</h4>
+                        <form method="POST" action="<?= Util::Print($_SERVER['REQUEST_URI']); ?>">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" placeholder="Username"
+                                    name="username" minlength="3" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-sm" placeholder="Password"
+                                    name="password" minlength="4" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-sm"
+                                    placeholder="Confirm password" name="confirmPassword" minlength="4" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" placeholder="email" name="email"
+                                    required>
+                            </div>
+                            <button class="btn btn-outline-primary btn-block" name="register" id="submit" type="submit"
+                                value="submit">
+                                Register
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
