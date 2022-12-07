4f8bedba2096e3b1bdf2797c6b53a98d8f36c564
EASV70R <75667950+EASV70R@users.noreply.github.com>
2022-12-07 17:13:40 +0100

    Update about.php

--- views/about.php
+++ views/about.php
@@ -1,6 +1,8 @@
 <?php
 require_once './cms/controllers/company.php';
 
+$companyData = new Company;
+
 Util::Header();
 Util::Navbar();
 ?>
@@ -8,21 +10,17 @@ Util::Navbar();
     <div class="container-xxl py-5">
         <div class="container">
             <div class="row g-5 align-items-center">
+                <?php foreach ($companyData->GetCompanyArray() as $row) : ?>
                 <div class="col-lg-6">
                     <div class="about-img position-relative overflow-hidden p-5 pe-0">
-                        <img class="img-fluid w-100" src="<?= (SITE_URL); ?>/assets/img/represent.jpg">
+                        <img class="img-fluid w-100" src="<?= Util::Print($row->image); ?>">
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <h1 class="display-5 mb-4">Meals catered to your body</h1>
-                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et
-                        eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet
-                    </p>
-                    <p><i class="fa fa-check text-primary me-3"></i>Tempor erat elitr rebum at clita</p>
-                    <p><i class="fa fa-check text-primary me-3"></i>Aliqu diam amet diam et eos</p>
-                    <p><i class="fa fa-check text-primary me-3"></i>Clita duo justo magna dolore erat amet</p>
-                    <a class="btn btn-primary rounded-pill py-3 px-5 mt-3" href="">Read More</a>
+                    <h1 class="display-5 mb-4"><?= Util::Print($row->name); ?></h1>
+                    <p class="mb-4"><?= Util::Print($row->desc); ?></p>
                 </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
